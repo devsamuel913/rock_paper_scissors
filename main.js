0dde786ad2cb02da8555710a69bf9423dfbb49b5
Nathan Madrid <devsamuelescobar913@gmail.com>
2024-06-24 23:03:03 -0600

    Create random numbers and link HTML to JS

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random()*3)+1;
+
+    if (randomNumber == 1){
+        console.log("Rock")
+    }else if(randomNumber == 2){
+        console.log("Paper")
+
+    }else if(randomNumber == 3){
+        console.log("Scissors")
+
+    }
+}
+
+getComputerChoice()
